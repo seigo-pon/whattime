9dbf84ae043217d60d288913d53806c8cdb8b102
seigo-pon <s15upon@Gmail.com>
2019-04-18 01:11:15 +0900

    Set color theme.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import 'vuetify/src/stylus/app.styl'
 
 Vue.use(Vuetify, {
   iconfont: 'md',
+  theme: {
+    primary: '#3f51b5',
+    secondary: '#e91e63',
+    accent: '#ffc107',
+    error: '#f44336',
+    warning: '#00bcd4',
+    info: '#607d8b',
+    success: '#2196f3'
+  }
 })
